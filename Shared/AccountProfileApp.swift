5ab065a2fbe566b9f1fb890f996c94cf5c931971
Mwanja Joel <joelsilverworks@gmail.com>
2021-06-05 15:17:57 +0300

    Initial Commit

---
+++ Shared/AccountProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccountProfileApp.swift
+//  Shared
+//
+//  Created by Mwanja Joel on 05/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct AccountProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
